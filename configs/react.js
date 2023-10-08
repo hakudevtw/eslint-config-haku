f7c3503f4c50b706be3bca2b7feb80c2bdc9c4f3
Haku <hakudevtw@gmail.com>
2023-10-09 00:31:17 +0800

    fix: disable unused rules and refactor folders

---
+++ configs/react.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "../lib/base.js",
+    "../lib/redux.js",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "no-restricted-imports": [
+      "error",
+      {
+        name: "react",
+        importNames: ["default"],
+        message: 'Use import { xxx } from "react"; instead',
+      },
+    ],
+  },
+};
