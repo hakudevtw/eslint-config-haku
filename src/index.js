438e7bdc85e47eaa24bf6ea6ac0272ae6ec31d18
Haku <hakudevtw@gmail.com>
2023-10-08 21:33:22 +0800

    test: basic eslint config package

---
+++ src/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ["./lib/base-config.js", "plugin:prettier/recommended"],
+};
---
+++ src/react.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "./lib/base-config.js-config.js",
+    "./lib/redux-config.js",
+    "plugin:prettier/recommended",
+  ],
+};
