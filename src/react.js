d6004cc07e42d702e0582e18d110e2c656fdfe52
Haku <hakudevtw@gmail.com>
2023-10-08 22:04:38 +0800

    refactor: move out files from src

--- src/react.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    "airbnb",
-    "airbnb-typescript",
-    "./lib/base-config.js-config.js",
-    "./lib/redux-config.js",
-    "plugin:prettier/recommended",
-  ],
-};
