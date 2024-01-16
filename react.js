38dd8836784ade26fd6c525fcd7781addc2c6e2a
Haku <hakudevtw@gmail.com>
2024-01-16 19:13:47 +0800

    feat: update rules and plugins configs

--- react.js
+++ react.js
@@ -1,11 +1,26 @@
 module.exports = {
-  extends: ["./lib/base", "airbnb-typescript", "plugin:tailwindcss/recommended", "prettier"],
+  extends: [
+    "./lib/base",
+    "plugin:react-hooks/recommended ",
+    "airbnb-typescript",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ],
+  plugins: ["react-refresh"],
   rules: {
+    "react-refresh/only-export-components": "warn",
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
     "react/jsx-props-no-spreading": "off",
     "react/require-default-props": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "jsx-a11y/label-has-associated-control": "off",
 
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      { js: "never", jsx: "never", ts: "never", tsx: "never" },
+    ],
     "no-restricted-imports": [
       "error",
       {
